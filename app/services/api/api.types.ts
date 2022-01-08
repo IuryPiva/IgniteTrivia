ba128e1d76a515f4459b0e9cc27ef8914de8c6be
Iury Piva <iurypiva@gmail.com>
2022-01-08 14:02:09 -0300

    add questions models

--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -1,13 +1,5 @@
+import { QuestionSnapshot } from "../../models/question/question"
 import { GeneralApiProblem } from "./api-problem"
-import { Character } from "../../models/character/character"
 
-export interface User {
-  id: number
-  name: string
-}
+export type GetQuestionsResult = { kind: "ok"; questions: QuestionSnapshot[] } | GeneralApiProblem
 
-export type GetUsersResult = { kind: "ok"; users: User[] } | GeneralApiProblem
-export type GetUserResult = { kind: "ok"; user: User } | GeneralApiProblem
-
-export type GetCharactersResult = { kind: "ok"; characters: Character[] } | GeneralApiProblem
-export type GetCharacterResult = { kind: "ok"; character: Character } | GeneralApiProblem
